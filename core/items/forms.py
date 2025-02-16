86a45716675ee6db335fa42c075ca924821299f8
jcheng-servicesolutions <jcheng@servicesolutions.biz>
2025-02-16 11:28:27 +0800

    update - num per unit TO packs per unit - labels only for now

--- core/items/forms.py
+++ core/items/forms.py
@@ -24,6 +24,9 @@ class ItemForm(forms.ModelForm):
         model = Item
         fields = ['name', 'excerpt', 'weight', 'unit',
                   'num_per_unit', 'company', 'price']
+        labels = {
+            "num_per_unit": "Packs/Unit"
+        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -44,6 +47,11 @@ class ItemForm(forms.ModelForm):
         # Validate price
         if price is not None and price < Decimal("0"):
             self.add_error("price", "Price cannot be negative.")
+
+        # Validate packs per unit
+        if num_per_unit is not None and num_per_unit < Decimal("1"):
+            self.add_error(
+                "num_per_unit", "Packs per unit needs to be atleast 1.")
 
         return cleaned_data
 
